5c4c258b151bb6731f1ec796f5628d7a47adbb38
anuragtewariji <anurag.tewariji@gmail.com>
2024-01-02 22:43:03 +0530

    Add files via upload

---
+++ button-toggle-mode-example.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Button toggle selection mode
+ */
+@Component({
+  selector: 'button-toggle-mode-example',
+  templateUrl: 'button-toggle-mode-example.html',
+})
+export class ButtonToggleModeExample {}
+
+
